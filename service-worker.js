7d4a2be36b8d2e9f66685ca36590214b2bb99a6e
vaibhavsindhu <vaibhav.sindhu@gmail.com>
2025-04-13 23:20:51 -0400

    added files

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('ipl-predict-v1').then(cache => {
+            return cache.addAll([
+                './index.html',
+                './manifest.json',
+                './icon.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
